5d46a69956e02fecf4ab3ab84ca4adb74b5399ae
Ram Narayan <ramn1997@outlook.com>
2026-02-07 12:50:51 +0530

    Remove old track player initialization code from index.js

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-
-// Try to register track player service if available
-try {
-    const TrackPlayer = require('react-native-track-player').default;
-    const { PlaybackService } = require('./src/services/TrackPlayerService');
-
-    if (TrackPlayer && TrackPlayer.registerPlaybackService) {
-        TrackPlayer.registerPlaybackService(() => PlaybackService);
-        console.log('[Player] Track Player service registered');
-    }
-} catch (e) {
-    console.log('[Player] react-native-track-player not available in Expo Go');
-}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
